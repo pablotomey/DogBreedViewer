a19b073be3331d979ab90ff1dac52f7f5f51a1a1
pablotomey <pablo.tp87@gmail.com>
2021-02-04 16:50:33 -0300

    implementación de librerías y arquitectura inicial

--- app/src/main/java/cl/konadev/dogbreedviewer/view/MainActivity.kt
+++ app/src/main/java/cl/konadev/dogbreedviewer/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package cl.konadev.dogbreedviewer
+package cl.konadev.dogbreedviewer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.konadev.dogbreedviewer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
